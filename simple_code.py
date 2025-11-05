bcda4948fedad741a04d1a02945066bf032dc87a
aysenurrakcan <aysenurakcan5181@gmail.com>
2025-11-05 19:37:36 +0300

    Update simple_code.py

--- simple_code.py
+++ simple_code.py
@@ -7,3 +7,24 @@ print(protein)
 
 output:
 IWP*FVG*KECASD
+
+
+#HAMMING DISTANCE
+def hamming_distance(a: str, b: str, pad_char: str = "_") -> int:
+   
+    a = a.lower()
+    b = b.lower()
+
+    max_len = max(len(a), len(b))
+    a_padded = a.ljust(max_len, pad_char)
+    b_padded = b.ljust(max_len, pad_char)
+
+    return sum(ch1 != ch2 for ch1, ch2 in zip(a_padded, b_padded))
+
+
+
+slack = "Ayşenur Akcan"
+twitter = "aysenura"
+
+distance = hamming_distance(slack, twitter)
+print("Hamming distance:", distance)
